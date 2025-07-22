9cb8cff115c31805338c35190934f7f37f7ec8b0
Ryan Clarian <rclarian@gmail.com>
2025-07-22 15:56:46 +0800

    #8. Vue JS 3 Tutorial - 8 - Binding Classes - (v-bind:class  directive)
    #8. Vue JS 3 Tutorial - 8 - Binding Classes - (v-bind:class  directive)

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -6,6 +6,17 @@
     <div v-html="hack"></div>
     <h2 v-bind:id="headingId">Heading</h2>
     <button v-bind:disabled="isDisabled">Bind</button>
+    <h2 class="underline">Underline text</h2>
+    <h2 class="underline" v-bind:class="status">Status</h2>
+    <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+    <h2 v-bind:class="isSoldout ? 'sold-out' : 'new'">Soldout? movie</h2>
+    <h2 v-bind:class="['new', 'promoted']">Newly promoted movie</h2>
+    <h2 v-bind:class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']">Array conditional movie</h2>
+    <h2 v-bind:class="{
+      promoted: isPromoted,
+      new: !isSoldout,
+      'sold-out': isSoldout
+    }">Object conditional movie</h2>
   </div>
 </template>
 
@@ -20,7 +31,10 @@ export default {
       channel: '<b>Codevolution</b>',
       hack: `<a href="#" onclick="alert('You have been hacked!')">Win a prize!</a>`,
       headingId: 'heading',
-      isDisabled: true,
+      isDisabled: false,
+      status: 'danger',
+      isPromoted: true,
+      isSoldout: true,
     };
   },
 }
@@ -34,5 +48,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.underline {
+  text-decoration: underline;
+}
+
+.promoted {
+  font-style: italic;
+}
+
+.new {
+  color: olivedrab;
+}
+
+.sold-out {
+  color: red;
 }
 </style>

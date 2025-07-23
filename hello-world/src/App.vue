dda951f8039aad85edda87e04787b87fb17746a1
Ryan Clarian <rclarian@gmail.com>
2025-07-23 19:06:55 +0800

    #16. Vue JS 3 Tutorial - 16 - Methods - (methods in vue)
    #16. Vue JS 3 Tutorial - 16 - Methods - (methods in vue)

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,64 +1,31 @@
 <template>
   <div>
-    <h2 v-for="(name, index) in names" :key="name">{{ index }} {{ name }}</h2>
-    <h2 v-for="name in fullNames" :key="name.first">{{ name.first }} {{ name.last }}</h2>
-    <div v-for="actor in actors" :key="actor.name">
-      <h2>{{ actor.name }}</h2>
-      <h3 v-for="movie in actor.movies" :key="movie">{{ movie }}</h3>
-    </div>
-    <h2 v-for="(value, key, index) in myInfo" :key="value">{{ index }} {{ key }} {{ value }}</h2>
-    <template v-for="name in names" :key="name">
-        <h2>{{ name }}</h2>
-        <hr />
-    </template>
-    <template v-for="name in names" :key="name">
-      <h2>{{ name }}</h2>
-      <input placeholder="Last name" />
-      <hr />
-    </template>
-    <button @click="shuffle">Shuffle!</button>
-
-    <template v-for="name in names" :key="name">
-      <h2 v-if="name === 'Bruce'">{{ name }}</h2>
-    </template>
-
+    <h2>{{ 2 + 3 + 5 }}</h2>
+    <h2>{{ 5 + 10 + 15 }}</h2>
+    <h2>Add method - {{ addNumber(2, 3, 5) }}</h2>
+    <h2>Add method - {{ addNumber(10, 15, 20) }}</h2>
+    <h2>Multiply method - {{ multiply(10) }}</h2>
+    <h2>Multiply method - {{ multiply(baseValue) }}</h2>
   </div>
 </template>
 
 <script>
-import _ from "lodash";
+
 export default {
   name: 'App',
   data(){
     return{
-      names: ['Bruce', 'Clark', 'Diana'],
-      fullNames: [
-        { first: 'Bruce', last: 'Wayne'},
-        { first: 'Clark', last: 'Kent'},
-        { first: 'Princess', last: 'Diana'},
-      ],
-      actors: [
-        {
-          name: 'Christian Bale',
-          movies: ['Batman', 'The Prestige'],
-        },
-        {
-          name: 'Di Caprio',
-          movies: ['Titanic', 'Inception'],
-        },
-      ],
-      myInfo: {
-        name: 'Vishwas',
-        channel: 'Codevolution',
-        course: 'Vue 3'
-      },
+      baseMultiplier: 5,
+      baseValue: 2,
     };
   },
   methods: {
-    shuffle() {
-      console.log(this.names);
-      this.names = _.shuffle(this.names);
+    addNumber(a, b, c){
+      return a + b + c;
     },
+    multiply(num) {
+      return num * this.baseMultiplier;
+    }
   },
 }
 </script>

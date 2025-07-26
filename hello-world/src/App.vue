a93450a5a08f927d2e72146c366f9538b5b04840
Ryan Clarian <rclarian@gmail.com>
2025-07-26 13:57:28 +0800

    #23. Vue JS 3 Tutoial - 23 - Computed Properties - (Computed Properties)
    #23. Vue JS 3 Tutoial - 23 - Computed Properties - (Computed Properties)

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
-    <h2 v-once>{{ name }}</h2>
-    <button @click="name = 'Batman'">Change Name</button>
-    <h2 v-pre>{{ name }}</h2>
+    <h2>Fullname - {{ firstName }} {{ lastName }}</h2>
+    <h2>Computed fullname - {{ fullName }}</h2>
+    <h2>
+      Total - {{ items.reduce((total, curr) => (total = total + curr.price), 0) }}
+    </h2>
+    <button @click="items.push({id: 4, title: 'Keyboard', price: 50})">Add Item</button>
+    <h2>Computed total - {{ total }}</h2>
   </div>
-  
 </template>
 
 <script>
@@ -13,12 +16,37 @@ export default {
   name: 'App',
   data(){
     return{
-      name: 'Vishwas'
+      firstName: 'Bruce',
+      lastName: 'Wayne',
+      items: [
+        {
+          id: 1,
+          title: 'TV',
+          price: 100,
+        },
+        {
+          id: 2,
+          title: 'Phone',
+          price: 200,
+        },
+        {
+          id: 3,
+          title: 'Laptop',
+          price: 300,
+        },
+      ]
     };
   },
   methods: {
-    
   },
+  computed: {
+    fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+    total() {
+      return this.items.reduce((total, curr) => (total = total + curr.price), 0);
+    }
+  }
 }
 </script>
 

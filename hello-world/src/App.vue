241a1f6e4fa1e8cb4b3a4760d9d8f97407ee0ac9
Ryan Clarian <rclarian@gmail.com>
2025-07-28 10:47:58 +0800

    #25. Vue JS 3 Tutorial - 25 - Computed Properties and v-for - (computed properties, v-for, v-if)
    #25. Vue JS 3 Tutorial - 25 - Computed Properties and v-for - (computed properties, v-for, v-if)

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -9,6 +9,15 @@
     <h2>Computed total - {{ total }}</h2>
     <h2>Method total - {{ getTotal() }}</h2>
     <input type="text" v-model="country">
+
+    <template v-for="item in items" :key="item.id">
+      <h2 v-if="item.price > 100">{{ item.title }} {{ item.price }}</h2>
+    </template>
+
+    <h2 v-for="item in expensiveItems" :key="item.id">
+      {{ item.title }} {{ item.price }}
+    </h2>
+
   </div>
 </template>
 
@@ -53,7 +62,10 @@ export default {
     total() {
       console.log('total computed property');
       return this.items.reduce((total, curr) => (total = total + curr.price), 0);
-    }
+    },
+    expensiveItems(){
+      return this.items.filter(item => item.price > 100);
+    },
   }
 }
 </script>

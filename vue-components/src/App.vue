f2b004d5d149713728fa13b1f62d667122fee04d
Ryan Clarian <rclarian@gmail.com>
2025-08-08 14:03:48 +0800

    #43. Vue JS 3 Tutorial - 43 - Teleport Component - (<teleport to="#portal-root">)
    #43. Vue JS 3 Tutorial - 43 - Teleport Component - (<teleport to="#portal-root">)

--- vue-components/src/App.vue
+++ vue-components/src/App.vue
@@ -1,34 +1,23 @@
 <template>
   <div>
-    <button @click="activeTab = 'TabA'">Tab A</button>
-    <button @click="activeTab = 'TabB'">Tab B</button>
-    <button @click="activeTab = 'TabC'">Tab C</button>
-
-    <keep-alive>
-      <component :is="activeTab"/>
-    </keep-alive>
-
-    <!-- <TabA v-if="activeTab === 'TabA'"/>
-    <TabB v-if="activeTab === 'TabB'"/>
-    <TabC v-if="activeTab === 'TabC'"/> -->
+    <teleport to="#portal-root">
+      <PortalRoot />
+    </teleport>
+    
   </div>
 </template>
 
 <script>
-import TabA from './components/TabA.vue';
-import TabB from './components/TabB.vue';
-import TabC from './components/TabC.vue';
+import PortalRoot from './components/PortalRoot.vue';
 
 export default {
   name: 'App',
   components: {
-    TabA,
-    TabB,
-    TabC
+    PortalRoot,
   },
   data() {
     return {
-      activeTab: 'TabA',
+      
     }
   },
   provide() {

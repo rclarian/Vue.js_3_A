dd0fb59e0b25810742617710ee39abacf30e878d
Ryan Clarian <rclarian@gmail.com>
2025-08-10 13:25:50 +0800

    #53. Vue JS 3 Tutorial - 53 - Replacing data with reactive - (ref('Clark'), reactive()
    #53. Vue JS 3 Tutorial - 53 - Replacing data with reactive - (ref('Clark'), reactive()

--- vue-components/src/components/DataView.vue
+++ vue-components/src/components/DataView.vue
@@ -1,29 +1,63 @@
 <template>
     <div>
         <h2>Options - {{ o_firstName }}</h2>
+        <h2>Options - {{ o_firstName }} {{ o_lastName }} a.k.a {{ o_heroName }}</h2>
+
         <h2>Composition - {{ c_firstName }}</h2>
         <h2>Composition - {{ greet }}</h2>
+
+        <h2>Composition - {{ c_firstName }} {{ c_lastName }} {{ c_heroName }}</h2>
+        <h2>Composition - {{ greetHero }}</h2>
+
+        <h2>Composition - {{ state.firstName }} {{ state.lastName }} {{ state.heroName }}</h2>
+        <h2>Composition - {{ reactiveGreetHero }}</h2>
+
     </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 
     export default {
         name: 'DataView',
         setup() {
             const c_firstName = ref('Clark');
-            c_firstName.value = 'Diana';
-            const greet = `Hello ${c_firstName.value}`
-            console.log('c_firstName', c_firstName);
+            const c_lastName = ref('Kent');
+            const c_heroName = ref('Superman');
+
+            const greet = `Hello ${c_firstName.value}`;
+            const greetHero = `Hello ${c_firstName.value} ${c_lastName.value} a.k.a ${c_heroName.value}`;
+
+            const state = reactive({
+                firstName: 'Princess',
+                lastName: 'Diana',
+                heroName: 'Wonder Woman'
+            })
+
+            const reactiveGreetHero = `Hello ${state.firstName} ${state.lastName} a.k.a. ${state.heroName}`
+
+            const isLoggedIn = ref(false);
+            const isloggedInReactive = reactive({
+                vallue: false,
+            })
+
             return {
                 c_firstName,
-                greet
+                c_lastName,
+                c_heroName,
+                greet, 
+                greetHero,
+                state,
+                reactiveGreetHero,
+                isLoggedIn, 
+                isloggedInReactive
             }
         },
         data() {
             return {
                 o_firstName: 'Bruce',
+                o_lastName: 'Wayne',
+                o_heroName: 'Batman',
             }
         },
     }

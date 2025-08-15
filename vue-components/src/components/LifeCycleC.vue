116b866f2f0d49a166b2ee7a45baa2ecb9db986c
Ryan Clarian <rclarian@gmail.com>
2025-08-15 20:35:44 +0800

    #61. Vue JS 3 Tutorial - 61 - Replacing Lifecycle Hooks
    #61. Vue JS 3 Tutorial - 61 - Replacing Lifecycle Hooks

---
+++ vue-components/src/components/LifeCycleC.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h3>Composition API Lifecycle Hooks</h3>
+    </div>
+</template>
+
+<script>
+import {
+    onBeforeMount,
+    onMounted,
+    onBeforeUpdate,
+    onUpdated,
+    onBeforeUnmount,
+    onUnmounted,
+} from 'vue'
+    export default {
+        name: 'LifeCycleC',
+        setup(){
+            onBeforeMount(() => {
+                console.log('LifeCycleC onBeforeMount()')
+            })
+            onMounted(() => {
+                console.log('LifeCycleC onMounted()')
+            })
+            onBeforeUpdate(() => {
+                console.log('LifeCycleC onBeforeUpdate()')
+            })
+            onUpdated(() => {
+                console.log('LifeCycleC onUpdated()')
+            })
+            onBeforeUnmount(() => {
+                console.log('LifeCycleC onBeforeUnmount()')
+            })
+            onUnmounted(() => {
+                console.log('LifeCycleC onUnmounted()')
+            })
+        }
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
